4a351feaef51daa97949233da0856159b0c1f563
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-01-30 11:06:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -681,3 +681,18 @@ function ReachedHeight(goal)
 
 Reset();
 window.requestAnimationFrame(GameLoop);
+
+
+//custom
+
+window.onbeforeunload = function (e) {
+    e = e || window.event;
+
+    // For IE and Firefox prior to version 4
+    if (e) {
+        e.returnValue = 'Sure?';
+    }
+
+    // For Safari
+    return 'Sure?';
+};
